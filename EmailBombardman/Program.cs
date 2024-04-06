c361a6b45597516b9a2939a207984e8cd8a5b06e
Babaefff <ilkinbabayevv777@gmail.com>
2024-04-06 10:16:16 +0400

    sdqwdqw

---
+++ EmailBombardman/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmailBombardman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string FromMail = "thedotnetchannelsender22@gmail.com";
+            string FromPassword = "lgioehkvchemfkrw";
+
+            MailMessage message = new MailMessage();    
+            message.From = new MailAddress(FromMail);
+            message.Subject = "Test mail";
+
+            message.Body = "Davaydaaa";
+            List<string> recipients = new List<string>
+{
+    "newsfromworld00@gmail.com",
+    "beyisi66@gmail.com",
+    "ilkinbaba20@gmail.com"
+    // Add more recipients as needed
+};
+            var smtpClient = new SmtpClient("smtp.gmail.com");
+            smtpClient.Port = 587;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(FromMail, FromPassword);
+            smtpClient.EnableSsl = true;
+            foreach (var recipient in recipients)
+            {
+                message.To.Clear(); // Clear previous recipients
+                message.To.Add(new MailAddress(recipient));
+
+                try
+                {
+                    smtpClient.Send(message);
+                    Console.WriteLine("Email sent to: " + recipient);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to send email to " + recipient + ": " + ex.Message);
+                }
+
+                Thread.Sleep(3000); // Wait for 3 seconds before sending the next email
+            }
+
+
+        }
+    }
+}
